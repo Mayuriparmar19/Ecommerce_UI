960c3730b30b53e75e3bd86e3f814a4ad1e2e013
System 16 <mittuparmar19@gmail.com>
2023-10-09 16:09:55 +0530

    Payment screen And switch Done

--- lib/Pages/bottom_nav.dart
+++ lib/Pages/bottom_nav.dart
@@ -9,29 +9,25 @@ import 'cat.dart';
 
 /// Flutter code sample for [BottomNavigationBar].
 
-
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
   @override
-  State<BottomNavBar> createState() =>
-      _BottomNavBarState();
+  State<BottomNavBar> createState() => _BottomNavBarState();
 }
 
-class _BottomNavBarState
-    extends State<BottomNavBar> {
+class _BottomNavBarState extends State<BottomNavBar> {
   int _selectedIndex = 0;
 
   List<Widget> screens1 = [
-  const Home(),
-  const Cart(),
-  const Profile(),
-
+    const Home(),
+    const Cart(),
+    const Profile(),
   ];
   static const List<Widget> screens = <Widget>[
-  Home(),
-  Cart(),
-  Profile(),
+    Home(),
+    Cart(),
+    Profile(),
   ];
 
   void _onItemTapped(int index) {
@@ -43,24 +39,40 @@ class _BottomNavBarState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
       body: Center(
         child: screens.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items:  <BottomNavigationBarItem>[
+        items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.square_grid_2x2,color:_selectedIndex==0? AppColors.selected: AppColors.secTextColor,size: 25,),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.cart,color: _selectedIndex==1? AppColors.selected: AppColors.secTextColor,size: 25,),
+            icon: Icon(
+              CupertinoIcons.square_grid_2x2,
+              color: _selectedIndex == 0
+                  ? AppColors.selected
+                  : AppColors.secTextColor,
+              size: 25,
+            ),
             label: '',
           ),
           BottomNavigationBarItem(
             icon: Icon(
-
-              CupertinoIcons.person,color:_selectedIndex==2? AppColors.selected: AppColors.secTextColor,size: 25,),
+              CupertinoIcons.cart,
+              color: _selectedIndex == 1
+                  ? AppColors.selected
+                  : AppColors.secTextColor,
+              size: 25,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.person,
+              color: _selectedIndex == 2
+                  ? AppColors.selected
+                  : AppColors.secTextColor,
+              size: 25,
+            ),
             label: '',
           ),
         ],
